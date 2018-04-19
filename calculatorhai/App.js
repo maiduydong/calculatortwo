c38cb14fad36244661e784f296140897bee13292
Mai Duy Dong <maiduydong94@gmail.com>
2018-04-19 09:51:21 +0700

    update

--- calculatorhai/App.js
+++ calculatorhai/App.js
@@ -2,76 +2,169 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+	Platform,
+	StyleSheet,
+	Text,
+	View
 } from 'react-native';
 import InputNumberButton from './src/InputNumberButton';
 
-const buttons =[
-  ['CLEAR','DEL'],
-  [7,8,9,':'],
-  [4,5,6,'x'],
-  [1,2,3,'-'],
-  [0,'.','=','+']
+
+const buttons = [
+	['CLEAR', 'DEL'],
+	['7', '8', '9', ':'],
+	['4', '5', '6', 'x'],
+	['1', '2', '3', '-'],
+	['0', '-', '=', '+']
 
 ];
 export default class App extends Component {
-  
-  renderButtons(){
-    let layouts = buttons.map((buttonRows,index)=>{
-      let rowItem = buttonRows.map((buttonItems, buttonIndex)=>{
-    return <InputNumberButton 
-    key ={'btn-' + buttonIndex}/>
-  
-    });
-  return <View style ={styles.inputRow}
-  key ={'row-' +index}>
-    {rowItem}
-  </View>
-    }
-  );
-  return layouts
-  }
-  render() {
-    return (
-      <View style={styles.container}>
+	constructor() {
+		super()
+		this.initialState = {
+			displayValue: '0',
+			operator: null,
+			firstValue: '',
+			secondValue: '',
+			nextValue: false
+		}
+		this.state = this.initialState;
+	}
+	renderButton() {
+		let layouts = buttons.map((buttonRows, index) => {
+			let rowItem = buttonRows.map((buttonItems, buttonIndex) => {
+				return <InputNumberButton
+					value={buttonItems}
+					handleOnPress={this.handleInput.bind(this, buttonItems)}
+					key={'btn-' + buttonIndex} />
 
-        <View style ={ styles.resultContainer}>
-          <Text style ={styles.resultText}>0</Text>
-        </View>
+			});
+			return <View style={styles.inputRow} key={'row-' + index}>{rowItem}</View>
+		});
+		return layouts
+	}
+	handleInput = (input) => {
+		const { displayValue, operator, firstValue, secondValue, nextValue } = this.state;
+		switch (input) {
+			case '0':
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
+				this.setState({
+					displayValue: (displayValue === '0') ? input : displayValue + input
+				})
+
+				if (!nextValue) {
+					this.setState({
+						firstValue: firstValue + input
+					})
+				} else {
+					this.setState({
+						secondValue: secondValue + input
+					})
+				}
+
+				break;
+			case '+':
+			case '-':
+			case 'x':
+			case ':':
+				this.setState({
+					nextValue: true,
+					operator: input,
+					displayValue: (operator !== null ? displayValue.substr(0, displayValue.length - 1) : displayValue) + input
+				})
+				break;
+			case '.':
+				let dot = displayValue.toString().slice(-1)
+				this.setState({
+					displayValue: dot !== '.' ? displayValue + input : displayValue
+				})
+
+				if (!nextValue) {
+					this.setState({
+						firstValue: firstValue + input
+					})
+				} else {
+					this.setState({
+						secondValue: secondValue + input
+					})
+				}
+				break;
+			case '=':
+				let formatOperator = (operator == 'x') ? '*' : (operator == ':') ? '/' : operator
+				let result = eval(firstValue + formatOperator + secondValue)
+				this.setState({
+					displayValue: result % 1 === 0 ? result : result.toFixed(2),
+					firstValue: result % 1 === 0 ? result : result.toFixed(2),
+					secondValue: '',
+					operator: null,
+					nextValue: false
+				})
+				break;
+
+			case 'CLEAR':
+				this.setState(this.initialState);
+				break;
+			case 'DEL':
+				let string = displayValue.toString();
+				let deletedString = string.substr(0, string.length - 1);
+				let length = string.length;
+				this.setState({
+					displayValue: length == 1 ? '0' : deletedString,
+					firstValue: length == 1 ? '' : deletedString
+				})
+				break;
+		}
 
 
-
-        <View style ={styles.inputContainer}>
-              {this.renderButtons()}
-        </View>
-      </View>
-    );
-  }
+	}
+	render() {
+		return (
+			<View style={styles.container}>
+				<View style={styles.resultContainer}>
+					<Text style={styles.resultText}>{this.state.displayValue}</Text>
+				</View>
+				<View style={styles.inputContainer}>
+					{this.renderButton()}
+				</View>
+			</View>);
+	}
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex:1,
-  },
-  resultContainer:{
-    flex: 2,
-    justifyContent:'center',
-    backgroundColor:'green',
+const styles = StyleSheet.create(
+	{
+		container: {
+			flex: 1
+		},
+		resultContainer: {
+			flex: 3,
+			justifyContent: 'center',
+			backgroundColor: 'green',
+			margin: 5
+		},
+		inputContainer: {
+			flex: 7,
+			backgroundColor: 'black'
 
-  },
-  inputContainer:{
-    flex: 8,
-    backgroundColor: 'yellow'
-  },
-  resultText:{
-    color: 'white',
-    fontWeight:'bold',
-    fontSize: 60,
-    padding: 20,
-    textAlign: 'right'
-  }
+		},
+		resultText: {
+			color: 'white',
+			fontSize: 40,
+			fontWeight: 'bold',
+			padding: 20,
+			textAlign: 'right'
+		},
+		inputRow: {
+			flex: 1,
+			flexDirection: 'row'
+		}
 
-});
+	}
+);
--- calculatorhai/src/InputNumberButton.js
+++ calculatorhai/src/InputNumberButton.js
@@ -12,25 +12,39 @@ import {
 
 
 export default class InputNumberButton extends Component {
-  render() {
-    return (
-      
-        <TouchableOpacity style ={styles.container}>
-
-        </TouchableOpacity>
-
-      
-    );
-  }
+	render(){
+		const {value, handleOnPress} = this.props;
+		
+		return(
+			<TouchableOpacity style = {styles.container}
+			OnPress={()=> handleOnPress(value)}>
+				<Text style ={styles.text}>{value}</Text>
+			</TouchableOpacity>
+		);
+	}
 }
 
-const styles = StyleSheet.create({
-    container: {
-      flex:1,
-      margin:1,
-      backgroundColor: 'gray',
+const styles = StyleSheet.create(
+  {
+    container:{
+      flex: 1,
+      margin:  1,
+      backgroundColor: 'yellow',
+      justifyContent: 'center',
+      borderRadius: 50,
       justifyContent:'center',
-      alignItems:'center'
+      alignItems: 'center',
+      borderColor: 'red',
+      borderWidth: 2
+    },
+    text:{
+      fontSize: 45,
+      color: 'black',
+      margin: 15
+      
+      
     }
-  
-  });
+    
+    
+  }
+  );
